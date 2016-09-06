31172d7703b38c9783712164d778c11b05fcfddd
Artem Zinoviev <arzinoviev@gmail.com>
2016-09-06 17:23:21 +0300

    ingredients list with loop

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,22 +1,18 @@
-import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {Component, Output, EventEmitter} from '@angular/core';
 import {Recipe} from "../recipe";
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: 'recipe-list.component.html'
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent {
 
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe("Schnitzel", "Very Tasty", "https://upload.wikimedia.org/wikipedia/commons/a/ae/Wiener-Schnitzel02.jpg", []),
+    new Recipe("Summer Salad", "Okayish", "http://thedivinemissmommy.com/wp-content/uploads/2012/07/ALDI-Summer-Salad-Watermelon-Feta-Mango-Raspberry-Dressing-e1341203561130.jpg", [])
+  ];
 
   @Output() recipeSelected = new EventEmitter<Recipe>();
-
-  recipe: Recipe = new Recipe("Dummy", "Dummy description", "https://placehold.it/350x250");
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   onSelected(recipe: Recipe) {
     this.recipeSelected.emit(recipe)
