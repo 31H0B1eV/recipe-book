bb255a5e77c95bb37b4bfb4da13e3e01b987daf4
Artem Zinoviev <arzinoviev@gmail.com>
2016-09-12 14:30:32 +0300

    implement adding to shopping list

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from "../recipes/shared/ingredient";
+import { ShoppingListService } from "./shopping-list.service";
 
 @Component({
   selector: 'rb-shopping-list-add',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListAddComponent implements OnInit {
 
-  constructor() { }
+  item: Ingredient;
+  isAdd = true;
+
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(ingredient: Ingredient) {
+    if(!this.isAdd) {
+
+    } else {
+      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.sls.addItem(this.item);
+    }
   }
 
 }
